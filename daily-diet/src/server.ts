e566f2ddb3612fc47fb8f2bbad2921affd0d9e52
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-07-15 12:45:44 -0300

    feat: add eslint

--- daily-diet/src/server.ts
+++ daily-diet/src/server.ts
@@ -1,14 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 const PORT = 3333
 
 const app = fastify()
 
-app.get('/hello',(req, res)=>{
-
-    return res.send({message: 'Hello world'})
+app.get('/hello', (req, res) => {
+  return res.send({ message: 'Hello world' })
 })
 
-app.listen({
+app.listen(
+  {
     port: PORT,
-
-}, ()=>console.log('😎 Server running in PORT ' + PORT + ' 😎'))
+  },
+  () => console.log('😎 Server running in PORT ' + PORT + ' 😎'),
+)
