d2f7d8da0b80f39f55c770ec3043e7f467028380
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-07-16 14:02:31 -0300

    chore: config database

---
+++ daily-diet/src/database.ts
@@ -0,0 +1,20 @@
+import { knex as setupKnex, Knex } from 'knex'
+import 'dotenv/config'
+import { env } from './env'
+
+export const config: Knex.Config = {
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+
+export const knex = setupKnex(config)
