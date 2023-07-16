633bfdf6b9142f288dab1d2c23b26e432032962a
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-07-16 20:11:40 -0300

    feat: add user session

--- daily-diet/src/routers/users.ts
+++ daily-diet/src/routers/users.ts
@@ -2,7 +2,9 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
-import { hash } from 'bcrypt'
+import { hash, compare } from 'bcrypt'
+import { AppError } from '../utils/errors'
+import { sign } from 'jsonwebtoken'
 
 export async function users(app: FastifyInstance) {
   app.post('/user', async (req, res) => {
@@ -28,9 +30,40 @@ export async function users(app: FastifyInstance) {
     return res.status(201).send()
   })
 
-  app.get('/users', async (req, res) => {
-    const transactions = await knex('users').select()
+  app.post('/user/session', async (req, res) => {
+    const createTransactionBodySchema = z.object({
+      email: z.string(),
+      password: z.string(),
+    })
 
-    return res.send(transactions)
+    const { email, password } = createTransactionBodySchema.parse(req.body)
+
+    const user = await knex('users').select().where({ email }).first()
+
+    if (!user) {
+      throw new AppError('Email or password incorrect!')
+    }
+
+    const passwordMatch = await compare(password, user.password)
+
+    if (!passwordMatch) {
+      throw new AppError('Email or password incorrect!')
+    }
+
+    const token = sign({}, 'mimi', {
+      subject: user.id,
+      expiresIn: '1d',
+    })
+
+    res.cookie('token', `Bearer ${token}`, {
+      path: '/',
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+    })
+  })
+
+  app.get('/users', async (req, res) => {
+    const users = await knex('users').select()
+
+    return res.send(users)
   })
 }
