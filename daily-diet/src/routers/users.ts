3cf0aeec6e638cfd731ed423a3b067f7cd410a4b
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-07-16 22:12:22 -0300

    feat: add an endpoint to view metrics for a specific user.

--- daily-diet/src/routers/users.ts
+++ daily-diet/src/routers/users.ts
@@ -5,6 +5,7 @@ import { randomUUID } from 'node:crypto'
 import { hash, compare } from 'bcrypt'
 import { AppError } from '../utils/errors'
 import { sign } from 'jsonwebtoken'
+import { authentication } from '../middlewares/authentication'
 
 export async function users(app: FastifyInstance) {
   app.post('/user', async (req, res) => {
@@ -64,4 +65,60 @@ export async function users(app: FastifyInstance) {
 
     return res.send(users)
   })
+
+  app.get(
+    '/users/metrics',
+    { preHandler: [authentication] },
+    async (req, res) => {
+      const { id } = req.user
+      const totalSnacks = await knex('snacks')
+        .select()
+        .where({ user_id: id })
+        .count('id', { as: 'total' })
+        .first()
+
+      const totalSnacksInDiet = await knex('snacks')
+        .select()
+        .where({ user_id: id, is_diet: true })
+        .count('id', { as: 'total' })
+        .first()
+
+      const totalSnacksOutDiet = await knex('snacks')
+        .select()
+        .where({ user_id: id, is_diet: false })
+        .count('id', { as: 'total' })
+        .first()
+
+      const snacks = await knex('snacks')
+        .select()
+        .where({ user_id: id })
+        .orderBy('created_at', 'asc')
+
+      let currentSequence = 0
+      let bestSequence = 0
+      let currentPosition = 0
+
+      for (const snack of snacks) {
+        if (snack.is_diet) {
+          currentSequence++
+        } else {
+          bestSequence =
+            currentSequence > bestSequence ? currentSequence : bestSequence
+          currentSequence = 0
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        currentPosition++
+      }
+
+      bestSequence =
+        currentSequence > bestSequence ? currentSequence : bestSequence
+
+      return res.send({
+        totalSnacks: totalSnacks?.total,
+        totalSnacksInDiet: totalSnacksInDiet?.total,
+        totalSnacksOutDiet: totalSnacksOutDiet?.total,
+        bestSequence,
+      })
+    },
+  )
 }
