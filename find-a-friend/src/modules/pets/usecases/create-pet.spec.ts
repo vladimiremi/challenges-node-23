09537da5e79d1a0ef7bce4d5f92ea1c6c4d524fd
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-09-09 15:56:09 -0300

    test: create a pet

---
+++ find-a-friend/src/modules/pets/usecases/create-pet.spec.ts
@@ -0,0 +1,31 @@
+import {beforeAll, beforeEach, describe, expect, it} from 'vitest'
+import { PetsRepository } from '../repositories/pets-repository'
+import { CreatePetUseCase } from './create-pet.usecase'
+import { InMemoryPetRepository } from '../repositories/in-memory/in-memory-pet-repository'
+
+
+let petRepository:InMemoryPetRepository
+let sut:CreatePetUseCase
+
+describe("Create a Pet", ()=>{
+
+    beforeEach(async ()=>{
+        petRepository = new InMemoryPetRepository()
+        sut = new CreatePetUseCase(petRepository)
+    })
+
+    it("Should be able create a Pet", async ()=>{
+
+        const pet = await sut.execute({
+            about: 'teste', 
+            address: 'teste', 
+            energy: 5, 
+            name: 'Vladimir', 
+            phone: '86981266700', 
+            size: 3,
+        })
+
+     expect(pet.id).toEqual(expect.any(String))
+    })
+
+})
